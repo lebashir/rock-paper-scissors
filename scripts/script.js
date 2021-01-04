7c0d0e99e9af2c83bc4d54e5a09c945c6374d4f7
Bashir <bashir.abihabib@gmail.com>
2021-01-04 16:11:04 -0600

    converted rock paper scissors to work with buttons and display results

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let enemy = document.querySelector('#computerScore');
+let player = document.querySelector('#playerScore')
+
 const result = document.querySelector('.result')
 
 const buttons = document.querySelectorAll('button');
@@ -10,7 +13,13 @@ buttons.forEach((button) => {
         console.log(playRound(e.target.value, computerPlay()));
     });
 })
-
+function checkWinner() {
+    if (playerScore == 3) {
+        return result.innerHTML="<span>YOU WIN YOU ARE AWESOME</span>";    
+    } else if (computerScore ==3){
+        return result.innerHTML="<span>PC OVERLORD WINS</span>"; 
+    }
+}
 // Computer choice function
 function computerPlay() {
     let roll = Math.floor(Math.random() * 3) + 1;
@@ -23,73 +32,64 @@ function computerPlay() {
     }
 }
        
-// player choice function
-    // function playRound(playerSelection, computerSelection) {
-    //     let lowercaseP = playerSelection.toLowerCase();
-    //     let lowercaseC = computerSelection.toLowerCase();
-    //     let playerScore = 0;
-    //     let computerScore = 0;
-        
-    //     if (lowercaseP == "rock" && lowercaseC == "scissors") {
-    //         playerScore ++;
-
-    //         return "You Win! Rock beats Scissors.";
-    //     } else if (lowercaseP == "rock" && lowercaseC == "paper") {
-    //         computerScore++;
-    //         return "You Lose! Paper beats Rock."
-    //     } else if (lowercaseP == "scissors" && lowercaseC == "paper") {
-    //         playerScore++;
-    //         return "You Win! Scissors beats Paper.";
-    //     } else if (lowercaseP == "scissors" && lowercaseC == "rock") {
-    //         computerScore++;
-    //         return "You Lose! Rock beats Paper."
-    //     } else if (lowercaseP == "paper" && lowercaseC == "rock") {
-    //         playerScore++;
-    //         return "You Win! Paper beats Rock."
-    //     } else if (lowercaseP == "paper" && lowercaseC == "scissors") {
-    //         computerScore++;
-    //         return "You Lose! Scissors beats Paper."
-    //     } else if (lowercaseP === lowercaseC) {
-    //         return "It's a tie, you both chose " + computerSelection + "!";
-    //     }
-    // }
-
+    // play Round and Update score;
     function playRound(playerSelection, computerSelection) {
         let lowercaseP = playerSelection.toLowerCase();
         let lowercaseC = computerSelection.toLowerCase();
-         if (playerScore == 3) {
-            result.innerHTML="<span>YOU WIN YOU ARE AWESOME</span>";
-            return;
-        } else if (computerScore ==3){
-            result.innerHTML="<span>PC OVERLORD WINS</span>";
-            return;
-        }
-         else{
+         
             if (lowercaseP == "rock" && lowercaseC == "scissors") {
+
                 playerScore= ++playerScore;
+                player.innerText= playerScore
+
                 result.innerHTML="<span>You Win! Rock beats Scissors.</span>";
+                
             } else if (lowercaseP == "rock" && lowercaseC == "paper") {
+
                 computerScore= ++computerScore;
+                enemy.innerText = computerScore;
+
                 result.innerHTML="<span>You Lose! Paper beats Rock.</span>" 
+
             } else if (lowercaseP == "scissors" && lowercaseC == "paper") {
+
                 playerScore= ++playerScore;
+                player.innerText = playerScore;
+
                 result.innerHTML="<span>You Win! Scissors beats Paper.</span>";
+
             } else if (lowercaseP == "scissors" && lowercaseC == "rock") {
+
                 computerScore= ++computerScore;
+                enemy.innerText = computerScore;
+
                 result.innerHTML="<span>You Lose! Rock beats Paper.</span>";
+
             } else if (lowercaseP == "paper" && lowercaseC == "rock") {
+
                 playerScore= ++playerScore;
+                player.innerText = playerScore;
+
                 result.innerHTML="<span>You Win! Paper beats Rock.</span>";
+
             } else if (lowercaseP == "paper" && lowercaseC == "scissors") {
+
                 computerScore= ++computerScore;
+                enemy.innerText = computerScore;
+
                 result.innerHTML="<span>You Lose! Scissors beats Paper.</span>";
+
             } else if (lowercaseP === lowercaseC) {
+
                 result.innerHTML='<span>Its a tie!</span>';
             }
-        } 
+            checkWinner();
         console.log(playerScore)
         console.log(computerScore)
     }
+
+    
+    
     // function game() {
 
     //     for (var i = 0; i < 5; i++) {
